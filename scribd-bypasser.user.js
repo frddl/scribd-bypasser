81d611554eea4553efb8565e53c8182f4b6c4846
Farid Mammadov <faridmmmdv@gmail.com>
2016-12-11 03:07:09 +0900

    Create scribd-bypasser.user.js

---
+++ scribd-bypasser.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name            Scribd: Bypasser
+// @description     At one point in our student life we all need to Google homework solutions, test preparation stuff and everything else somehow related to our academic life. One of the search results is, usually, Scribd.com, where to download the file you either need to upload bunch of documets, or get a monthly subscription. Too lazy? I'm as well. This extension allows you to bypass all that procedure on the Scribd, so you can download everything easily!
+// @namespace       http://github.com/frddl/scribd-bypasser
+// @version         1.0.0
+// @author          Farid Mammadov
+// @license         MIT
+// @released        2016-12-11
+// @updated         2016-12-11
+// @match           *://*.scribd.com/*
+// @match           *://www.scribd.com/*
+// @run-at          document-end
+// ==/UserScript==
+
+window.onload = function () {
+	console.log("log working!")
+	alert("working!");
+}
